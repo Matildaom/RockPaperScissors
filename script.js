2601c8d22af57e890bf8fb0ebe15f0a11f7ef588
Matildaom <x24152412@student.ncirl.ie>
2024-11-12 22:27:18 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+function play(playerChoice) {
+    const choices = ['rock', 'paper', 'scissors'];
+    const computerChoice = choices[Math.floor(Math.random() * 3)];
+        //I don't fully understand this code^. Array??
+
+        //Tells what computers choice is
+    document.getElementById("computerChoice").innerHTML= computerChoice;
+   
+
+    let outcome = '';
+
+    if (playerChoice == computerChoice) {
+        outcome = "That's a draw!!";
+    } else if (
+        (playerChoice == 'rock' && computerChoice == 'scissors') ||
+        (playerChoice == 'paper' && computerChoice == 'rock') ||
+        (playerChoice == 'scissors' && computerChoice == 'paper')
+    ) {
+        outcome = "You win!";
+    } else {
+        outcome = "You lose!";
+    }
+
+    document.getElementById('outcome').textContent = outcome;
+}
